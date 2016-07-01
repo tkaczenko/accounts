c6fb58d5488b7067551665b7427bff75ad8ca2e0
Andrii Tkachenko <include.free@gmail.com>
2016-07-01 15:47:09 +0300

    Add tests

--- src/test/scala/org/tests/accounts/spec/DisableSpec.scala
+++ src/test/scala/org/tests/accounts/spec/DisableSpec.scala
@@ -1,12 +1,25 @@
 package org.tests.accounts.spec
 
+import org.accounts.models.{MasterJsonProtocol, ResponseString}
 import org.accounts.routes.AccountRoute
 import org.scalatest.FlatSpec
 import org.scalatest.concurrent.ScalaFutures
+import spray.http.ContentTypes._
+import spray.http.HttpEntity
 import spray.routing.HttpService
 import spray.testkit.ScalatestRouteTest
 
 class DisableSpec extends FlatSpec with ScalatestRouteTest with HttpService with AccountRoute with ScalaFutures {
+  import MasterJsonProtocol._
+
   val url = "/disable"
   def actorRefFactory = system
+
+  it should ("return JSON response with code 200") in {
+    Post(url, HttpEntity(`application/json`,
+      """{"id":1}""".stripMargin)
+    ) ~> routs ~> check {
+      responseAs[ResponseString] === ResponseString(code = 200, message = "Success")
+    }
+  }
 }
