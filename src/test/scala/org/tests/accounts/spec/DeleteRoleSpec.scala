7d31d6524343f6942b826fa1841fe2b142aff75f
Andrii Tkachenko <include.free@gmail.com>
2016-07-02 04:30:20 +0300

    Add several tests

---
+++ src/test/scala/org/tests/accounts/spec/DeleteRoleSpec.scala
@@ -0,0 +1,29 @@
+package org.tests.accounts.spec
+
+import org.accounts.models.{MasterJsonProtocol, ResponseString}
+import org.accounts.routes.AccountRoute
+import org.scalatest.FlatSpec
+import org.scalatest.concurrent.ScalaFutures
+import spray.http.ContentTypes._
+import spray.http.HttpEntity
+import spray.routing.HttpService
+import spray.testkit.ScalatestRouteTest
+
+/**
+  * Created by tkacz- on 02.07.16.
+  */
+class DeleteRoleSpec extends FlatSpec with ScalatestRouteTest with HttpService with AccountRoute with ScalaFutures {
+  import MasterJsonProtocol._
+
+  val url = "/delete_role"
+  def actorRefFactory = system
+
+  it should ("return JSON response with code 200") in {
+    Post(url, HttpEntity(`application/json`,
+      """{"id":1,
+        |"name":"Some role"}""".stripMargin)
+    ) ~> routs ~> check {
+      responseAs[ResponseString] === ResponseString(code = 200, message = "Success")
+    }
+  }
+}
